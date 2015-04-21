dc0d2aff7c40a02c3669329e2fda96a9e83acda8
Travis <travisneely@gmail.com>
2015-04-21 02:41:13 -0500

    Initial texture loading code and a texture plane created.

--- openglrender/openglrender.cpp
+++ openglrender/openglrender.cpp
@@ -2,6 +2,80 @@
 
 #include "openglrender.h"
 
+
+//Loads Bitmap into memory and Loads it
+////// Texture Information
+BITMAPINFOHEADER bitmapInfoHeader; // bitmap info header
+unsigned char* bitmapData; // the texture data
+unsigned int texture; // the texture object
+WORD BITMAP_ID = 19778;
+
+
+unsigned char *LoadBitmapFile(char *filename, BITMAPINFOHEADER *bitmapInfoHeader)
+{
+    FILE *filePtr; // the file pointer
+    BITMAPFILEHEADER bitmapFileHeader; // bitmap file header
+    unsigned char *bitmapImage; // bitmap image data
+    int imageIdx = 0; // image index counter
+    unsigned char tempRGB; // swap variable
+
+    // open filename in "read binary" mode
+    filePtr = fopen(filename, "rb");
+    if (filePtr == NULL)
+        return NULL;
+
+    // read the bitmap file header
+    fread(&bitmapFileHeader, sizeof(BITMAPFILEHEADER), 1, filePtr);
+
+    // verify that this is a bitmap by checking for the universal bitmap id
+    if (bitmapFileHeader.bfType != BITMAP_ID)
+    {
+        qDebug()<<"LoadBitmapFile - BITMAP_ID invalid";
+        fclose(filePtr);
+        return NULL;
+    }
+    // read the bitmap information header
+    fread(bitmapInfoHeader, sizeof(BITMAPINFOHEADER), 1, filePtr);
+
+    // move file pointer to beginning of bitmap data
+    fseek(filePtr, bitmapFileHeader.bfOffBits, SEEK_SET);
+
+    // allocate enough memory for the bitmap image data
+    bitmapImage = (unsigned char*)malloc(bitmapInfoHeader->biSizeImage);
+
+    // verify memory allocation
+    if (!bitmapImage)
+    {
+        free(bitmapImage);
+        fclose(filePtr);
+        return NULL;
+    }
+
+    // read in the bitmap image data
+    fread(bitmapImage, 1, bitmapInfoHeader->biSizeImage, filePtr);
+
+    // make sure bitmap image data was read
+    if (bitmapImage == NULL)
+    {
+        fclose(filePtr);
+        return NULL;
+    }
+
+    // swap the R and B values to get RGB since the bitmap color format is in BGR
+    for (imageIdx = 0; imageIdx < bitmapInfoHeader->biSizeImage; imageIdx+=3)
+    {
+        tempRGB = bitmapImage[imageIdx];
+        bitmapImage[imageIdx] = bitmapImage[imageIdx + 2];
+        bitmapImage[imageIdx + 2] = tempRGB;
+    }
+
+    // close the file and return the bitmap image data
+    fclose(filePtr);
+    return bitmapImage;
+}
+
+
+
 //openGLRender(QWidget *parent  = 0);
 openGLRender::openGLRender(QWidget *parent) : QGLWidget(QGLFormat(QGL::SampleBuffers), parent)
 {
@@ -23,20 +97,23 @@ void openGLRender::initializeGL(){
 
     GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
     GLfloat mat_shininess[] = { 50.0 };
-    GLfloat light0_position[] = { 0.0, 0.0, 1.0, 0.0 };
+    GLfloat light0_position[] = { 0.0, 0.0, 10.0, 0.0 };
     glClearColor (0.0, 0.0, 0.0, 0.0);
     glShadeModel (GL_SMOOTH);
 
-    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
-    //glLightfv(GL_LIGHT0, GL_AMBIENT, light0_position);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mat_specular);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, mat_shininess);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light0_position);
     glLightfv(GL_LIGHT0, GL_POSITION, light0_position);
+    glLightfv(GL_LIGHT0, GL_AMBIENT_AND_DIFFUSE, light0_position);
 
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LEQUAL);                 // The type of depth testing to do
     glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+    glEnable(GL_TEXTURE_2D);
 
     //setBackgroundColor(255,255,255,255);
     //glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -47,7 +124,7 @@ void openGLRender::initializeGL(){
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
 
-    glOrtho(-1.0f, 1.0f, -1.0f, 1.0f, -1.0f, 1.0f);
+    glOrtho(-10.0f, 10.0f, -10.0f, 10.0f, -2.0f, 2.0f);
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
@@ -88,6 +165,41 @@ void openGLRender::paintGL(){
 */
     draw();
 
+
+    //Create terrain
+    //g_glRender->Prepare(g_hiResTimer->GetElapsedSeconds(1));
+    //g_glRender->Render();
+
+    //glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // clear to black
+
+    glShadeModel(GL_SMOOTH); // use smooth shading
+    glEnable(GL_DEPTH_TEST); // hidden surface removal
+    glEnable(GL_CULL_FACE); // do not calculate inside of poly's
+    glFrontFace(GL_CCW); // counter clock-wise polygons are out
+
+    glEnable(GL_TEXTURE_2D); // enable 2D texturing
+    //change wood.bmp to the name of your bitmap
+    // load our bitmap file
+    bitmapData = LoadBitmapFile("D:\\Projects\\Qt\\battleships\\tga\\water.bmp", &bitmapInfoHeader);
+
+    glGenTextures(1, &texture); // generate texture object
+    glBindTexture(GL_TEXTURE_2D, texture); // enable our texture object
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+    // generate the texture image
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, bitmapInfoHeader.biWidth,
+                 bitmapInfoHeader.biHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, bitmapData);
+    glBegin(GL_QUADS); // front face
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(20.0f, -20.0f, 0.0f);
+    glTexCoord2f(5.0f, 0.0f); glVertex3f(20.0f, 20.0f, 0.0f);
+    glTexCoord2f(5.0f, 5.0f); glVertex3f(-20.0f, 20.0f, 0.0f);
+    glTexCoord2f(0.0f, 5.0f); glVertex3f(-20.0f, -20.0f, 0.0f);
+    glEnd();
+
+
+
     makeCurrent();
     this->swapBuffers();
 
@@ -105,7 +217,7 @@ void openGLRender::draw(){
                 if(((openGLPolygon*)objects->at(i))->isFinished())
                     ((openGLPolygon*)objects->at(i))->drawFilled();
 
-            if(objects->at(i)->name() != "openGLPolygon")
+            if(objects->at(i)->name() != "openGLPolygon" && objects->at(i)->name() != "openGLMesh")
                 objects->at(i)->draw();
         }
 
@@ -251,3 +363,14 @@ void divide_tetra(GLfloat *a, GLfloat *b, GLfloat *c, GLfloat *d, int m)
     }
     else(tetra(a,b,c,d)); /* draw tetrahedron at end of recursion */
 }
+
+
+
+
+
+
+
+
+
+
+
