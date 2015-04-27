7000014a6ac955bf3fb5bb647645255feff19f1d
JET005 <ethanwtsn.watson5@gmail.com>
2015-04-27 09:16:06 -0500

    added data structures for bitmap to work on my computer

--- openglrender/openglrender.h
+++ openglrender/openglrender.h
@@ -16,6 +16,33 @@
 
 #define GAME_MODE 0
 #define MENU_MODE 1
+#define DWORD  unsigned int
+#define LONG  int
+#define UINT  unsigned int
+#define WORD  unsigned short int
+
+typedef struct tagBITMAPFILEHEADER {
+  WORD    bfType;
+  DWORD   bfSize;
+  WORD    bfReserved1;
+  WORD    bfReserved2;
+  DWORD   bfOffBits;
+} BITMAPFILEHEADER, *PBITMAPFILEHEADER;
+
+
+typedef struct tagBITMAPINFOHEADER{
+  DWORD  biSize;
+  LONG   biWidth;
+  LONG   biHeight;
+  WORD   biPlanes;
+  WORD   biBitCount;
+  DWORD  biCompression;
+  DWORD  biSizeImage;
+  LONG   biXPelsPerMeter;
+  LONG   biYPelsPerMeter;
+  DWORD  biClrUsed;
+  DWORD  biClrImportant;
+} BITMAPINFOHEADER, *PBITMAPINFOHEADER;
 
 class openGLRender : public QGLWidget
 {
--- openglrender/ship.h
+++ openglrender/ship.h
@@ -8,6 +8,13 @@ class ship : public openGLMesh
 public:
     ship();
     ~ship();
+    getVelocity();
+    increaseAcceleration();
+    decreaseAcceleration();
+
+private:
+    float acceleration, velocity,angle;
+
 };
 
 #endif // SHIP_H
