70862e07f730840adb9ea7b13e5c4e95809090ab
JET005 <ethanwtsn.watson5@gmail.com>
2015-05-07 21:26:43 -0500

    something

--- openglrender/ship.cpp
+++ openglrender/ship.cpp
@@ -210,10 +210,16 @@ void ship::update(const int &msec)
             trigger++;
     }
     else{
+        if(this->translation[1]>20){
+            qDebug()<<"ship out of bounds";
+        }
+
+        else{
         this->translation[0] -= ((2.0f * msec/100)*sin(this->rotation[2]*3.14159265/180));
         //this->translation[1] += 1.0f * msec/1000;
         this->translation[1] += ((2.0f * msec/100)*cos(this->rotation[2]*3.14159265/180));
         //((ship*)o)->translate(trans[0]-(0.1f*sin(rot[2]*3.14159265/180)), trans[1]+(0.1f*cos(rot[2]*3.14159265/180)), trans[2]);
+    }
     }
 }
 
