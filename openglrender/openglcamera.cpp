fbb0bb5283e90a3605e03a442e95fa3d663b0f8f
Travis <travisneely@gmail.com>
2015-05-13 01:09:02 -0500

    Merge camera conflicts

--- openglrender/openglcamera.cpp
+++ openglrender/openglcamera.cpp
@@ -18,7 +18,6 @@ openGLCamera::~openGLCamera()
 
 void openGLCamera::init()
 {
-<<<<<<< HEAD
     GLfloat mat_specular[] = { 0.508273f, 0.508273f, 0.508273f, 1.0f };
     GLfloat mat_shininess[] = { 0.4 };
     GLfloat light0_ambient[] = { 0.19225f, 0.19225f, 0.19225f, 1.0f };
@@ -30,34 +29,7 @@ void openGLCamera::init()
     glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, light0_ambient);
     glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, light0_diffuse);
     glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mat_specular);
-=======
-    GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 10.0 };
-    GLfloat mat_shininess[] = { 100.0 };
 
-   GLfloat global_ambient[]={0.2,0.2,0.2,1.0};
-
-    GLfloat light0_ambient[] = { .2, .2, .2, 1.0 };
-    GLfloat light0_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
-    GLfloat light0_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-    GLfloat light0_position[] = { 10.0, 10.0, -5.0, 1.0 };
-
-    GLfloat light1_ambient[] = { .20, .20, .20, 1.0 };
-    GLfloat light1_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
-    GLfloat light1_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-    GLfloat light1_position[] = { 10.0, -10.0, -5.0, 1.0 };
-
-    GLfloat light2_ambient[] = { .20, .20, .20, 1.0 };
-    GLfloat light2_diffuse[] = { 1.0,1.0, 1.0, 1.0 };
-    GLfloat light2_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-    GLfloat light2_position[] = { -10.0, -10.0, -5.0, 1.0 };
-
-    GLfloat light3_ambient[] = { .20, .20, .20, 1.0 };
-    GLfloat light3_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
-    GLfloat light3_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-    GLfloat light3_position[] = { -10.0, 10.0, -5.0, 1.0 };
-
-    glMaterialfv(GL_FRONT, GL_AMBIENT, mat_specular);
->>>>>>> origin/master
     glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, mat_shininess);
 
     glLightfv(GL_LIGHT0, GL_LINEAR_ATTENUATION, light0_diffuse);
@@ -71,37 +43,18 @@ void openGLCamera::init()
     glLightfv(GL_LIGHT1, GL_POSITION, light1_position);
 
     glLightfv(GL_LIGHT0, GL_LINEAR_ATTENUATION, light0_diffuse);
-    glLightfv(GL_LIGHT1, GL_AMBIENT, light1_ambient);
-    glLightfv(GL_LIGHT1, GL_DIFFUSE, light1_diffuse);
-    glLightfv(GL_LIGHT1, GL_SPECULAR, light1_specular);
+    glLightfv(GL_LIGHT1, GL_AMBIENT, light0_ambient);
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, light0_diffuse);
+    glLightfv(GL_LIGHT1, GL_SPECULAR, light0_specular);
     glLightfv(GL_LIGHT1, GL_POSITION, light1_position);
 
-    glLightfv(GL_LIGHT0, GL_LINEAR_ATTENUATION, light0_diffuse);
-    glLightfv(GL_LIGHT2, GL_AMBIENT, light2_ambient);
-    glLightfv(GL_LIGHT2, GL_DIFFUSE, light2_diffuse);
-    glLightfv(GL_LIGHT2, GL_SPECULAR, light2_specular);
-    glLightfv(GL_LIGHT2, GL_POSITION, light2_position);
-
-    glLightfv(GL_LIGHT0, GL_LINEAR_ATTENUATION, light0_diffuse);
-    glLightfv(GL_LIGHT3, GL_AMBIENT, light3_ambient);
-    glLightfv(GL_LIGHT3, GL_DIFFUSE, light3_diffuse);
-    glLightfv(GL_LIGHT3, GL_SPECULAR, light3_specular);
-    glLightfv(GL_LIGHT3, GL_POSITION, light3_position);
-
-    glLightModelfv(GL_LIGHT_MODEL_AMBIENT,global_ambient);
+    //glLightModelfv(GL_LIGHT_MODEL_AMBIENT,global_ambient);
 
     glEnable(GL_LIGHTING);
     //glEnable(GL_DEPTH_TEST);
     glEnable(GL_LIGHT0);
     glEnable(GL_LIGHT1);
-<<<<<<< HEAD
-=======
-    glEnable(GL_LIGHT2);
-    glEnable(GL_LIGHT3);
 
-
-
->>>>>>> origin/master
 
 }
 
